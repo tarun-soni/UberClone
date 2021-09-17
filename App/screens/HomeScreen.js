418216faef42723c6dcc39d0af4eaad73c857641
tarun-soni <soni.tarun26@gmail.com>
2021-09-17 19:11:49 +0530

    added api and enabled location autocomplete

--- App/screens/HomeScreen.js
+++ App/screens/HomeScreen.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Image, SafeAreaView, StyleSheet, View } from 'react-native'
 
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import tw from 'tailwind-react-native-classnames'
+import { GOOGLEMAPS_API_KEY } from '@env'
 import NavOptions from '../components/NavOptions'
 
 const styles = StyleSheet.create({
@@ -22,7 +24,32 @@ const HomeScreen = () => {
             uri: 'https://links.papareact.com/gzs',
           }}
         />
-
+        <View style={{ borderWidth: 4 }}>
+          <GooglePlacesAutocomplete
+            styles={{
+              container: {
+                flex: 0,
+              },
+              textInput: {
+                fontSize: 18,
+              },
+            }}
+            enablePoweredByContainer={false}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+            placeholder="Search"
+            fetchDetails
+            onPress={(data, details = null) => {
+              // 'details' is provided when fetchDetails = true
+              console.log('GMAPI', data, details)
+            }}
+            query={{
+              key: GOOGLEMAPS_API_KEY,
+              language: 'en',
+            }}
+            onFail={e => console.log(`error in GMAPS`, e)}
+          />
+        </View>
         <NavOptions />
       </View>
     </SafeAreaView>
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   plugins: [
+//     [
+//       'module:react-native-dotenv',
+//       {
+//         moduleName: '@env',
+//         path: '.env',
+//       },
+//     ],
+//   ],
+// }
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [['module:react-native-dotenv']],
 }
